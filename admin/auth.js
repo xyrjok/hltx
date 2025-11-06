85dafc722b5eab06f60a64ea5072c4e5d62608cd
xyrjok <yyl9821@163.com>
2025-11-07 04:10:32 +0800

    Delete admin directory

--- admin/auth.js
+++
@@ -1,7 +0,0 @@
-// 这是一个共享脚本，用于检查所有后台页面的登录状态
-// 如果未登录，它会
-// 自动重定向到登录页。
-
-if (!sessionStorage.getItem('admin-token') && !window.location.pathname.endsWith('/admin/index.html')) {
-    window.location.href = '/admin/index.html';
-}
